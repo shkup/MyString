888b8466251ba63a224fa21a9d052d2222bd7107
Shkop <yaronshkop@hotmail.com>
2020-04-04 19:57:30 +0300

    fixes

--- Project1/MyString.h
+++ Project1/MyString.h
@@ -37,11 +37,7 @@ public:
 	// Indication if there is data or not in our string
 	bool isEmpty() const;
 
-	/* TODO: Not sure about it at ALL. I think it's better to return char[] which is a full copy!!! of the data in other memory segment. 
-	   Although the pointer is constant it's still exposing the internal implementation...
-	   Moreover... You use this function to check other instances private data which is bad practice.
-	   Strings need to be immutable by definition. Deep copy ensures no one will mess with your pointer.
-	*/
+	// The string value (immutable pointer)
 	const char* value() const;
 
 	// Assignment operator. 
@@ -62,9 +58,7 @@ public:
 	// Concatenate operator
 	MyString& operator+(const MyString& other);
 
-	/* TODO: I don't remember ostream, istream... 
-	Generally speaking you MUST have a VERY GOOD reason to let someone friend accessibility!
-	Friend is violating encapsulation so there must be some very exceptional good reason for this...*/
+	// cout and cin helpers
 	friend ostream& operator<<(ostream& os, const MyString& string);
 	friend istream& operator >> (istream& is, MyString& string);
 };
